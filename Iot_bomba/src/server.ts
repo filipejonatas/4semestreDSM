e5303e117cc2b62b7d9c9367003c8097a56a5b35
filipejonatas <jonatas.filipe.ferreira@gmail.com>
2025-05-06 21:23:47 -0300

    ajuste no servidor

---
+++ Iot_bomba/src/server.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { pool } from './connection';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+interface EventoBomba {
+  id: number;
+  status: string;
+  data_acao: string;
+}
+
+app.post('/evento', async (req: Request, res: Response) => {
+  const { status, data_acao } = req.body;
+
+  if (!status) {
+    res.status(400).json({ error: 'Status é obrigatório.' });
+  }
+
+  try {
+    const result = await pool.query<EventoBomba>(
+      'INSERT INTO evento_bomba (status, data_acao) VALUES ($1, $2) RETURNING *',
+      [status, data_acao ? new Date(data_acao) : new Date()]
+    );
+    res.status(201).json(result.rows[0]);
+    console.log("bomba acionada")
+  } catch (err) {
+    console.error('Erro ao salvar evento:', err);
+    res.status(500).json({ error: 'Erro ao salvar evento.' });
+  }
+});
+
+app.get('/evento', async (_req: Request, res: Response) => {
+  try {
+    const result = await pool.query<EventoBomba>('SELECT * FROM evento_bomba ORDER BY data_acao DESC');
+    res.json(result.rows);
+    
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao buscar eventos.' });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(3000, '0.0.0.0', () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
